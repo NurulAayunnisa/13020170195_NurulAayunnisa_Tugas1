abdbc742f0c78b799064a090e69e0d1f19aed913
NurulAayunnisa <annisa.nhurul@gmail.com>
2020-05-12 09:08:23 -0700

    Tugas1

---
+++ app/src/main/java/com/example/latihanlab/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.example.latihanlab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        btn = findViewById(R.id.btnPindah);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Main2Activity.this, Main3Activity.class));
+            }
+        });
+    }
+}
